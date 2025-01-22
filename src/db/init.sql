d8cd76aff93d8950975f758ebc86faca6fd023f5
gigatexal <alex@alexandarnarayan.com>
2025-01-22 21:28:27 +0100

    wip

--- src/db/init.sql
+++ src/db/init.sql
@@ -5,4 +5,20 @@ this.author = author;
 this.authorId = authorId;
 this.message = message;
 this.timestamp = timestamp;
+
+simple schema to store and make leaderboard style queries easier
+
 */
+
+CREATE TABLE channels (
+    id INT PRIMARY KEY, -- system generated, internal key
+    chan_id INT NOT NULL, -- the natural-key from discord
+    is_active BOOL DEFAULT TRUE,
+    deactivated_at TEXT DEFAULT NULL,
+    created_at TEXT DEFAULT 
+) STRICT;
+
+CREATE TABLE messages (
+    id int PRIMARY KEY,
+    
+)
